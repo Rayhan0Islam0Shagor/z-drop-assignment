cb25ebf544a8bcdf3839bc9b053eff489a2a5722
Rayhan Islam <67514865+Rayhan0Islam0Shagor@users.noreply.github.com>
2021-07-27 01:59:07 +0600

    summary pannel added

--- components/OrderSummary/OrderSummary.tsx
+++ components/OrderSummary/OrderSummary.tsx
@@ -1,9 +1,60 @@
+import { useState } from 'react';
 import styles from './OrderSummary.module.scss';
+import * as IoIcons from 'react-icons/io';
 
 const OrderSummary = () => {
+  const [visible, setVisible] = useState(true);
+
+  const handleClick = () => {
+    setVisible(!visible);
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className={styles.order__summary}>
-      <h1>Order Summary</h1>
+      <div className={styles.headline}>
+        <p>Order Summary</p>
+      </div>
+
+      <div className={styles.price__section}>
+        <div className={styles.price}>
+          <p>
+            Subtotal <span>(20 items)</span>
+          </p>
+          <p>BDT. 38,850</p>
+        </div>
+        <div className={styles.price}>
+          <p>Discount</p>
+          <p>BDT. 0</p>
+        </div>
+        <div className={styles.price}>
+          <p>Shipping Charge</p>
+          <p>BDT. 600</p>
+        </div>
+      </div>
+
+      <div className={styles.options}>
+        <p>Gift Card, Coupons & Promotional Codes</p>
+
+        {visible ? (
+          <IoIcons.IoIosArrowDown onClick={handleClick} />
+        ) : (
+          <IoIcons.IoIosArrowUp onClick={handleClick} />
+        )}
+      </div>
+
+      <form className={styles.coupon__apply__form} onSubmit={submitHandler}>
+        <input type="text" placeholder="Voucher Code" />
+        <button type="submit">Apply</button>
+      </form>
+
+      <div className={styles.total}>
+        <p>Total</p>
+        <p>BDT. 39,375</p>
+      </div>
     </div>
   );
 };
--- pages/cart.tsx
+++ pages/cart.tsx
@@ -3,9 +3,6 @@ import CartBody from '../components/CartBody/CartBody';
 import Navbar from '../components/Navbar/Navbar';
 import SubNav from '../components/SubNav/SubNav';
 
-import { NavbarLinks } from '../data/data';
-import { NavLinks } from '../types/types';
-
 const Cart = () => {
   return (
     <>
